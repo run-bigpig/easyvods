d53baa13f71ff078b3746c5a133e09b7222680f3
run-bigpig <syskeykala@gmail.com>
2022-01-04 21:28:31 +0800

    修改数据库配置在config下mysql.php

---
+++ config/mysql.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ *Author:Syskey
+ *Date:2022/1/4
+ *Time:21:22
+ **/
+
+return [
+    'host' => '127.0.0.1',
+    'port' => 3306,
+    'database' => 'easyvods',
+    'username' => 'easyvods',
+    'password' => '123123',
+];
